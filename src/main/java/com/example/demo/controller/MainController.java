b26211a84299e563f879c10fa1e176273dc31078
Ekaterina Akulenko <akule011@gmail.com>
2022-12-13 18:04:59 +0400

    new version

--- src/main/java/com/example/demo/controller/MainController.java
+++ src/main/java/com/example/demo/controller/MainController.java
@@ -1,14 +1,19 @@
 package com.example.demo.controller;
 
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MainController {
+    static final Logger log = LogManager.getLogger(MainController.class);
 
     @GetMapping("/main")
     public String getMain(@RequestParam String param) {
+        log.info(param);
         return param;
     }
 }
