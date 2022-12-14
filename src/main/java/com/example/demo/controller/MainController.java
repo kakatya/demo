0ffc31f85904f7a9b5f36de114e4754642045680
Ekaterina Akulenko <akule011@gmail.com>
2022-12-14 16:01:52 +0400

    new version

--- src/main/java/com/example/demo/controller/MainController.java
+++ src/main/java/com/example/demo/controller/MainController.java
@@ -1,6 +1,5 @@
 package com.example.demo.controller;
 
-
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.web.bind.annotation.GetMapping;
